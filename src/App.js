ba77b6df10ea8adb5bdd5c110e97e2877e2ed370
Henry <aguhenrychuks@gmail.com>
2022-11-10 22:01:17 +0100

    feat:navbar added including responsiveness

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+
+// React Router
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div>
+          <Switch>
+            <Route exact path="/">
+              <Home/>
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,14 @@
 import React, { useState } from "react";
+
+// stylesheet
 import "./Navbar.css";
+
+// Router Links
+import { Link } from 'react-router-dom'
+
+// Images
+import Logo1 from "../images/Logo1.png";
+import Logo2 from "../images/Logo2.png";
 function Navbar() {
   const [showMenu, setShowMenu] = useState(false);
   return (
@@ -8,7 +17,12 @@ function Navbar() {
         <nav className="navbar is-transparent is-light has-background-white">
           <div className="container">
             <div className="navbar-brand">
-              <a className="navbar-item" href="/"></a>
+              <Link className="navbar-item" to="/">
+                <div className="logo">
+                    <img src={Logo1} alt="Logo1" />
+                    <img src={Logo2} alt="Logo2" />
+                </div>
+              </Link>
               <button
                 className={"navbar-burger" + (showMenu ? " is-active" : "")}
                 onClick={() => setShowMenu(!showMenu)}
@@ -23,39 +37,38 @@ function Navbar() {
               id="navMenu"
             >
               <div className="navbar-end">
-                <a
+                <Link
                   className="navbar-item has-text-weight-medium"
-                  href="/login"
-                  style={{ color: "black" }}
+                  to="/"
+                  style={{ color: "#434343" }}
                 >
                   Home
-                </a>
-                <a
+                </Link>
+                <Link
                   className="navbar-item has-text-weight-medium"
-                  href="/login"
-                  style={{ color: "black" }}
+                  to="/place-to-stay"
+                  style={{ color: "#434343" }}
                 >
                   Place to stay
-                </a>
-                <a
+                </Link>
+                <Link
                   className="navbar-item has-text-weight-medium"
-                  href="/login"
-                  style={{ color: "black" }}
+                  to="/nfts"
+                  style={{ color: "#434343" }}
                 >
                   NFTs
-                </a>
-                <a
+                </Link>
+                <Link
                   className="navbar-item has-text-weight-medium"
-                  href="/login"
-                  style={{ color: "black" }}
+                  to="/community"
+                  style={{ color: "#434343" }}
                 >
                   Community
-                </a>
-                <a className="navbar-item has-text-weight-medium" href="/login">
-                  <button>Connect wallet</button>
-                  <i class="fa-solid fa-arrow-right-long"></i>
-                </a>
+                </Link>
               </div>
+              <Link className="navbar-item has-text-weight-medium" to="/connect-wallet">
+                  <button>Connect wallet</button>
+                </Link>
             </div>
           </div>
         </nav>
