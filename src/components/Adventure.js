336dae7d870d1ecd2b8ec2c7b7e67fb4ac133764
Henry <aguhenrychuks@gmail.com>
2022-11-11 10:29:44 +0100

    feat:footer updated for desktop

--- src/components/Adventure.js
+++ src/components/Adventure.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 // Images
 import card1 from "../images/card1.png";
 import card2 from "../images/card2.png";
@@ -10,140 +11,64 @@ import card8 from "../images/card8.png";
 import stars from "../images/stars.png";
 import "./Adventure.css";
 const Adventure = () => {
+  const [nfts, setNfts] = useState([
+    {
+      id: 1,
+      image: card1,
+    },
+    {
+      id: 2,
+      image: card2,
+    },
+    {
+      id: 3,
+      image: card3,
+    },
+    {
+      id: 4,
+      image: card4,
+    },
+    {
+      id: 5,
+      image: card5,
+    },
+    {
+      id: 6,
+      image: card6,
+    },
+    {
+      id: 7,
+      image: card7,
+    },
+    {
+      id: 8,
+      image: card8,
+    },
+  ]);
   return (
     <div className="adventure">
       <div className="adventure-header">
         <h1>Inspiration for your next adventure</h1>
       </div>
       <div className="adventure-box">
-        <div className="adventure-card">
-          <img src={card1} alt="card1" />
-          <div className="adventure-card-text">
-            <div className="left-text">
-              <p>Desert king</p>
-              <p>2345km away</p>
+        {nfts.map((nft, index) => (
+          <div className="adventure-card" key={nft.id}>
+            <img src={nft.image} alt="card1" />
+            <div className="adventure-card-text">
+              <div className="left-text">
+                <p>Desert king</p>
+                <p>2345km away</p>
+              </div>
+              <div className="right-text">
+                <p className="duration">1MBT per night</p>
+                <p>available for 2weeks stay</p>
+              </div>
             </div>
-            <div className="right-text">
-              <p className="duration">1MBT per night</p>
-              <p>available for 2weeks stay</p>
-            </div>
-          </div>
-          <div className="rating">
-            <img src={stars} alt="stars" />
-          </div>
-        </div>
-        <div className="adventure-card">
-          <img src={card2} alt="card2" />
-          <div className="adventure-card-text">
-            <div className="left-text">
-              <p>Desert king</p>
-              <p>2345km away</p>
-            </div>
-            <div className="right-text">
-              <p className="duration">1MBT per night</p>
-              <p>available for 2weeks stay</p>
+            <div className="rating">
+              <img src={stars} alt="stars" />
             </div>
           </div>
-          <div className="rating">
-            <img src={stars} alt="stars" />
-          </div>
-        </div>
-        <div className="adventure-card">
-          <img src={card3} alt="card3" />
-          <div className="adventure-card-text">
-            <div className="left-text">
-              <p>Desert king</p>
-              <p>2345km away</p>
-            </div>
-            <div className="right-text">
-              <p className="duration">1MBT per night</p>
-              <p>available for 2weeks stay</p>
-            </div>
-          </div>
-          <div className="rating">
-            <img src={stars} alt="stars" />
-          </div>
-        </div>
-        <div className="adventure-card">
-          <img src={card4} alt="card4" />
-          <div className="adventure-card-text">
-            <div className="left-text">
-              <p>Desert king</p>
-              <p>2345km away</p>
-            </div>
-            <div className="right-text">
-              <p className="duration">1MBT per night</p>
-              <p>available for 2weeks stay</p>
-            </div>
-          </div>
-          <div className="rating">
-            <img src={stars} alt="stars" />
-          </div>
-        </div>
-        <div className="adventure-card">
-          <img src={card5} alt="card5" />
-          <div className="adventure-card-text">
-            <div className="left-text">
-              <p>Desert king</p>
-              <p>2345km away</p>
-            </div>
-            <div className="right-text">
-              <p className="duration">1MBT per night</p>
-              <p>available for 2weeks stay</p>
-            </div>
-          </div>
-          <div className="rating">
-            <img src={stars} alt="stars" />
-          </div>
-        </div>
-        <div className="adventure-card">
-          <img src={card6} alt="card6" />
-          <div className="adventure-card-text">
-            <div className="left-text">
-              <p>Desert king</p>
-              <p>2345km away</p>
-            </div>
-            <div className="right-text">
-              <p className="duration">1MBT per night</p>
-              <p>available for 2weeks stay</p>
-            </div>
-          </div>
-          <div className="rating">
-            <img src={stars} alt="stars" />
-          </div>
-        </div>
-        <div className="adventure-card">
-          <img src={card7} alt="card7" />
-          <div className="adventure-card-text">
-            <div className="left-text">
-              <p>Desert king</p>
-              <p>2345km away</p>
-            </div>
-            <div className="right-text">
-              <p className="duration">1MBT per night</p>
-              <p>available for 2weeks stay</p>
-            </div>
-          </div>
-          <div className="rating">
-            <img src={stars} alt="stars" />
-          </div>
-        </div>
-        <div className="adventure-card">
-          <img src={card8} alt="card8" />
-          <div className="adventure-card-text">
-            <div className="left-text">
-              <p>Desert king</p>
-              <p>2345km away</p>
-            </div>
-            <div className="right-text">
-              <p className="duration">1MBT per night</p>
-              <p>available for 2weeks stay</p>
-            </div>
-          </div>
-          <div className="rating">
-            <img src={stars} alt="stars" />
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
